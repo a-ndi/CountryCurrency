b0220254ae8fdaf770b6dfbf0cfd4c0d3ca68de6
endy <ndifrekedem1@gmail.com>
2025-10-28 22:52:36 +0100

    Added Fonts

--- src/main/java/com/currency/CountryCurrency/CountryCurrencyApplication.java
+++ src/main/java/com/currency/CountryCurrency/CountryCurrencyApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CountryCurrencyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CountryCurrencyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        System.setProperty("java.awt.headless", "true");
+        SpringApplication.run(CountryCurrencyApplication.class, args);
+    }
 
 }
